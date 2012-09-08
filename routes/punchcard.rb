0d4a78edefec8f40e0895d522d05c7289eb079fc
Andrew Budd <andrew.budd@gmail.com>
2012-09-08 15:05:26 -0400

    missing file

---
+++ routes/punchcard.rb
@@ -0,0 +1,114 @@
+class Punchcard < Sinatra::Application
+
+	before do
+		content_type :json
+	end
+
+	get '/person/?' do
+		people = Person.all()
+		@Response.result = people.as_json(:exclude => @allExclude)
+		@Response.to_response
+	end
+
+	get '/person/:id/?' do
+		person = Person.get(params[:id])
+		if(person.nil?)
+			@Response.add_error :type => "Invalid_id", :message => "No person found with id = #{params[:id]}"
+		else
+			@Response.result = people.as_json(:exclude => @allExclude)
+		end
+		@Response.to_response
+	end
+
+	get '/teaser/?' do
+		teasers = Teaser.all()
+		@Response.result = teasers.as_json(:exclude => @allExclude)
+		@Response.to_response
+	end
+
+	get '/teaser/:id/?' do
+		teaser = Teaser.get(params[:id])
+		if(teaser.nil?)
+			@Response.add_error :type => "Invalid_id", :message => "No teaser found with id = #{params[:id]}"
+		else
+			@Response.result = teaser.as_json(:exclude => @allExclude)
+		end
+		@Response.to_response
+	end
+
+	put '/person/:id/?' do
+		puts params
+		["splat","captures"].each {|k| params.delete(k)}
+		put_or_post_person(params)
+	end
+
+	post '/person/?' do
+		[:id].each { |k| params.delete(k) }
+		put_or_post_person(params)
+	end
+
+	def put_or_post_person(params)
+		puts params
+		query_params = {}
+		
+		if(!params.include?("phone")) 
+			@Response.add_error :type => "Required_field", :message => "the phone field is required!"
+		end
+
+		params.each { |key, value|
+      
+     			# check if param is permitted
+			if (Person.properties.named?(key))
+        
+			        # Validate integer values
+		        	if (Person.properties[key].class == DataMapper::Property::Integer)
+			          begin
+        			    Integer(value)
+			          rescue ArgumentError => e
+        			    @Response.add_error :type => "Invalid_Type", :message => "Value of '#{key}' must be an integer type"
+	        		  end
+	        		end
+        
+        			query_params[key.to_sym] = value
+		 	else
+				@Response.add_error :type => "Invalid_Parameter", :message => "Parameter '#{key}' is invalid"
+			end
+		}		
+		fragment = nil
+
+		if(!@Response.is_error?)
+    			puts query_params
+			# check if we have an id to update (PUT request)
+			if(!query_params[:id].nil?)
+				person = Person.get(query_params[:id])
+            			if(fragment.nil?)
+                			@Response.add_error :type => "Not_Found", :message => "No person found with id = #{query_params[:id]}"
+            			else
+   			            Person.transaction do
+			                   if(!person.destroy() || !(person = Person.new(query_params)).save)
+                			        @Response.add_error :type => "Transaction_Failed", :message=>"Transaction for person id #{query_params[:id]} failed" 
+		        	           end
+	               		    end
+	            		end
+		        else #create a new one and save it
+            			person = Person.new(query_params)
+			        if(!person.save)
+			                @Response.add_error :type => "Save_Failed", :message => "Person failed to save #{fragment.errors.inspect}"
+		        end
+	            status 201
+        	end
+        	# if nothing has gone wrong, return the object
+	        if(!@Response.is_error?)
+	            @Response.result = fragment.as_json(:exclude => @allExclude)
+        	end
+	    else
+            if(response.status == 200)
+            	status 400
+	    end
+    end
+
+    @Response.to_response
+	
+	end
+
+end
