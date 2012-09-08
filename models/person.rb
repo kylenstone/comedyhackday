762a3ffc0cf81470dc0ed5cd536a84661f51e898
Andrew Budd <andrew.budd@gmail.com>
2012-09-08 15:26:28 -0400

    fixes

--- models/person.rb
+++ models/person.rb
@@ -2,7 +2,7 @@ class Person
 	include DataMapper::Resource
 	property :id,		Serial, :key=>true
 	property :phone,	String, :required => true
-	property :teaser_id	Integer, :required => true
+	property :teaser_id	Number, :required => true
 	
 	property :modified_at,	DateTime
 	property :created_at,	DateTime
