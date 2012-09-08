4ff6cd5979c46057eab2f55892985cf8078686d1
Andrew Budd <andrew.budd@gmail.com>
2012-09-08 14:02:29 -0400

    improvements?

--- models/init.rb
+++ models/init.rb
@@ -9,8 +9,7 @@ env = ENV["RACK_ENV"] || "development"
 #DataMapper::Model.raise_on_save_failure = true
 #DataMapper::Logger.new($stdout, :debug)
 #DataMapper.setup(:default, dbConfig[env])
-puts "Using database URL: " + ENV['DATABASE_URL']
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 #DataMapper.setup(:default,DATABASE_URL)
 #DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/development.sqlite3")
 
