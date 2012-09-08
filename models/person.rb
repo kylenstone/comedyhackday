ecfcec0f73e4909a1e22c047d2c9aea391c538af
Andrew Budd <andrew.budd@gmail.com>
2012-09-08 15:15:06 -0400

    posting for teasers

--- models/person.rb
+++ models/person.rb
@@ -1,9 +1,6 @@
 class Person
 	include DataMapper::Resource
 	property :id,		Serial, :key=>true
-	property :firstname,	String, :required => true
-	property :lastname, 	String, :required => true
-	property :email,	String, :required => true
 	property :phone,	String, :required => true
 	
 	property :modified_at,	DateTime
--- routes/punchcard.rb
+++ routes/punchcard.rb
@@ -93,7 +93,7 @@ class Punchcard < Sinatra::Application
 		        else #create a new one and save it
             			person = Person.new(query_params)
 			        if(!person.save)
-			                @Response.add_error :type => "Save_Failed", :message => "Person failed to save #{fragment.errors.inspect}"
+			                @Response.add_error :type => "Save_Failed", :message => "Person failed to save #{person.errors.inspect}"
 		        end
 	            status 201
         	end
@@ -110,5 +110,85 @@ class Punchcard < Sinatra::Application
     @Response.to_response
 	
 	end
+
+	put '/teaser/:id/?' do
+		puts params
+		["splat","captures"].each {|k| params.delete(k)}
+		put_or_post_person(teaser)
+	end
+
+	post '/teaser/?' do
+		[:id].each { |k| params.delete(k) }
+		put_or_post_person(teaser)
+	end
+
+	def put_or_post_teaser(params)
+		puts params
+		query_params = {}
+		
+		if(!params.include?("question")) 
+			@Response.add_error :type => "Required_field", :message => "the phone field is required!"
+		end
+
+		if(!params.include?("response")) 
+			@Response.add_error :type => "Required_field", :message => "the phone field is required!"
+		end
+
+		params.each { |key, value|
+      
+     			# check if param is permitted
+			if (Teaser.properties.named?(key))
+        
+			        # Validate integer values
+		        	if (Teaser.properties[key].class == DataMapper::Property::Integer)
+			          begin
+        			    Integer(value)
+			          rescue ArgumentError => e
+        			    @Response.add_error :type => "Invalid_Type", :message => "Value of '#{key}' must be an integer type"
+	        		  end
+	        		end
+        
+        			query_params[key.to_sym] = value
+		 	else
+				@Response.add_error :type => "Invalid_Parameter", :message => "Parameter '#{key}' is invalid"
+			end
+		}		
+		fragment = nil
+
+		if(!@Response.is_error?)
+    			puts query_params
+			# check if we have an id to update (PUT request)
+			if(!query_params[:id].nil?)
+				teaser = Teaser.get(query_params[:id])
+            			if(fragment.nil?)
+                			@Response.add_error :type => "Not_Found", :message => "No person found with id = #{query_params[:id]}"
+            			else
+   			            Person.transaction do
+			                   if(!teaser.destroy() || !(teaser = Teaser.new(query_params)).save)
+                			        @Response.add_error :type => "Transaction_Failed", :message=>"Transaction for teaser id #{query_params[:id]} failed" 
+		        	           end
+	               		    end
+	            		end
+		        else #create a new one and save it
+            			teaser = Teaser.new(query_params)
+			        if(!teaser.save)
+			                @Response.add_error :type => "Save_Failed", :message => "Teaser failed to save #{teaser.errors.inspect}"
+		        end
+	            status 201
+        	end
+        	# if nothing has gone wrong, return the object
+	        if(!@Response.is_error?)
+	            @Response.result = fragment.as_json(:exclude => @allExclude)
+        	end
+	    else
+            if(response.status == 200)
+            	status 400
+	    end
+    end
+
+    @Response.to_response
+	
+	end
+
 
 end
