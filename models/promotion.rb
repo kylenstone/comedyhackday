15b8c614cff78695cfa4e8b3ff5895e2ba90f93d
Andrew Budd <andrew.budd@gmail.com>
2012-09-09 10:24:04 -0400

    forgot file

---
+++ models/promotion.rb
@@ -0,0 +1,12 @@
+class Promotion
+	include DataMapper::Resource
+	property :id,		Serial, :key=>true
+	property :thing,	Text, :required => true
+	property :response,	Text, :required => true
+	property :code,		String, :required => true
+	property :claimed_by_person_id,	Integer, :required => true
+
+	property :modified_at,	DateTime
+	property :created_at,	DateTime
+end
+
