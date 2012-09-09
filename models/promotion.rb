ca0fd87cd0b5f785405ea20c817f267e31b72022
Andrew Budd <andrew.budd@gmail.com>
2012-09-09 10:26:38 -0400

    now correct

--- models/promotion.rb
+++ models/promotion.rb
@@ -2,9 +2,9 @@ class Promotion
 	include DataMapper::Resource
 	property :id,		Serial, :key=>true
 	property :thing,	Text, :required => true
-	property :response,	Text, :required => true
 	property :code,		String, :required => true
-	property :claimed_by_person_id,	Integer, :required => true
+	property :claimed_by_person_id,	Integer
+	property :time_to_claim, DateTime
 
 	property :modified_at,	DateTime
 	property :created_at,	DateTime
