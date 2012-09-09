3d6318dd85a79fa9c5287ccdf6220dc7b71734df
Andrew Budd <andrew.budd@gmail.com>
2012-09-08 21:40:53 -0400

    db fix

--- models/teaser.rb
+++ models/teaser.rb
@@ -2,8 +2,8 @@ class Teaser
 	include DataMapper::Resource
 	property :id,		Serial, :key=>true
 	property :type,		String, :required => true, :default => 'joke'
-	property :question,	String, :required => true
-	property :response,	String, :required => true
+	property :question,	Text, :required => true
+	property :response,	Text, :required => true
 
 	property :modified_at,	DateTime
 	property :created_at,	DateTime
