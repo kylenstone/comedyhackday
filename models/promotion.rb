187053384f545cd0b2930751dd0357fa7955e14c
Andrew Budd <andrew.budd@gmail.com>
2012-09-09 10:27:41 -0400

    fixes

--- models/promotion.rb
+++ models/promotion.rb
@@ -4,7 +4,7 @@ class Promotion
 	property :thing,	Text, :required => true
 	property :code,		String, :required => true
 	property :claimed_by_person_id,	Integer
-	property :time_to_claim, DateTime
+	property :time_to_claim, DateTime, :required => true 
 
 	property :modified_at,	DateTime
 	property :created_at,	DateTime
